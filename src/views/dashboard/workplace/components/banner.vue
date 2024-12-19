7e362cb6059253a872c7f8316de0175c1c74c9bc
lm10 <liaoming010@gmail.com>
2024-12-19 20:12:12 +0800

    add banner component in workplace page

---
+++ src/views/dashboard/workplace/components/banner.vue
@@ -0,0 +1,22 @@
+<template>
+  <a-col class="banner">
+    <a-col>
+      <a-typography-title :heading="5">{{ $t('workplace.welcome') }} admin</a-typography-title>
+    </a-col>
+    <a-divider class="panel-border"></a-divider>
+  </a-col>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="less" scoped>
+.banner {
+  width: 100%;
+  padding: 20px 20px 0 20px;
+  background-color: var(--color-gb-2);
+  border-radius: 4px 4px 0 0;
+}
+
+.panel-border{
+}
+</style>
--- src/views/dashboard/workplace/index.vue
+++ src/views/dashboard/workplace/index.vue
@@ -1,14 +1,41 @@
 <template>
-  <div>
-    workplace
+  <div class="container">
+    <div class="left-side">
+      <div class="panel">
+        <Banner />
+      </div>
+    </div>
+    <div class="right-side">right side</div>
   </div>
 </template>
 
 <script setup lang="ts">
-
+import Banner from './components/banner.vue'
 </script>
 
 <style scoped>
+.container {
+  display: flex;
+  background-color: var(--color-fill-2);
+  padding: 16px 20px;
+  padding-bottom: 0;
+}
 
+.left-side {
+  flex: 1;
+  overflow: hidden;
+}
+.right-side {
+  width: 280px;
+  margin-left: 16px;
+}
+.panel {
+  background-color: var(--color-bg-2);
+  border-radius: 4px;
+  overflow: auto;
+}
+:deep(.panel-border) {
+  margin-bottom: 0;
+  border-bottom: 1px solid rgb(var(--gray-2));
+}
 </style>
-
